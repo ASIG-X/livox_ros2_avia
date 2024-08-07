0f87e11e9a6c257cc15ace9a2b804de6795c2f15
Kailai Li <kailai.li@rug.nl>
2024-08-07 16:01:56 +0200

    added overide

--- livox_ros2_avia/launch/livox_lidar_msg_launch.py
+++ livox_ros2_avia/launch/livox_lidar_msg_launch.py
@@ -8,7 +8,7 @@ import launch
 xfer_format   = 1    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar,1-hub
-publish_freq  = 10.0 # freqency of publish,1.0,2.0,5.0,10.0,etc
+publish_freq  = 20.0 # freqency of publish,1.0,2.0,5.0,10.0,etc
 output_type   = 0
 frame_id      = 'livox_frame'
 lvx_file_path = '/home/livox/livox_test.lvx'
